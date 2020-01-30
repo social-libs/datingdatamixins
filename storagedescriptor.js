cedbaa227bd65dfe1ce49a0c7cf82c8c3e708c87
Andrija Petrovic <andrija.hers@gmail.com>
2020-01-30 17:09:01 +0000

    Init

---
+++ storagedescriptor.js
@@ -0,0 +1,17 @@
+module.exports = {
+  record: {
+    fields: [{
+      name: 'gender',
+      type: 'string'
+    },{
+      name: 'orientation',
+      type: 'number'
+    },{
+      name: 'birthdaytimestamp',
+      type: 'number'
+    },{
+      name: 'bio',
+      type: 'string'
+    }]
+  }
+};
---
+++ visiblefields/user.js
@@ -0,0 +1,1 @@
+module.exports = ['gender', 'orientation', 'birthdaytimestamp', 'bio'];
